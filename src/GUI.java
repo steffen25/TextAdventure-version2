e79e95cb6c59ac8d140b8846b31f94eb1a8fe8b1
Wumaniizer <danniwu93@gmail.com>
2014-11-24 10:47:39 +0100

    BLAH
    BLAH!

--- src/GUI.java
+++ src/GUI.java
@@ -20,6 +20,9 @@ public class GUI extends JFrame {
         initComponents();
     }
 
+    //TEST STEFFEN DIN FAG!!
+    
+
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
