0649f361c397c22b5db35ef366b4f0c758e8720a
Chrvang <chrvang89@gmail.com>
2014-11-24 11:25:03 +0100

    Create Items.java

---
+++ src/Items.java
@@ -0,0 +1,26 @@
+package TextAdventure;
+
+public class Items {
+
+	private String id;
+	private String description;
+
+	/**
+	 * 
+	 * @param id
+	 * @param description
+	 */
+	public Items(String id, String description) {
+	}
+
+	public void canUnlock() { //can be used to unlock
+	}
+
+	public void canLight() { //can be used to light
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
+
+}
