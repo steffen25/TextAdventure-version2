b21c041ed4bf22abecc1d16b75b19750a989cf4c
crellee <christianhasselstrom@10.111.180.90>
2014-11-25 10:12:09 +0100

    ny klasse med items (nøgle sværd lygte)

---
+++ src/RunItems.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author christianhasselstrom
+ */
+public class RunItems {
+    
+    public RunItems() {
+        
+        Items item1 = new Items("Item One","Key");
+        Items item2 = new Items("Item two", "Flash Light");
+        Items item3 = new Items("Item three", "Sword");
+        
+        
+        ArrayList<Items> items = new ArrayList<Items>();
+    }
+    
+}
