448414e7879a88876cd0b4fb7ae7e05d991f7535
steffen25 <acbolic@gmail.com>
2014-11-27 09:25:50 +0100

    new

--- src/Inventory/InventoryClass.java
+++ src/Inventory/InventoryClass.java
@@ -1,8 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package Inventory;
+import Room.Items;
+import java.util.ArrayList;
 
 /**
- * Created by danniwu on 25/11/14.
+ *
+ * @author Steffen
  */
 public class InventoryClass {
 
+    
+    //private ArrayList<Items> itemList;
+    
+    public ArrayList<Items> myItems()    {
+    ArrayList<Items> itemList = new ArrayList<Items>();
+
+    return(itemList);
+}
+    
+    //constuctor
+    public InventoryClass()
+    {
+        
+    }
+    
+    public boolean addItems(Items newItem) {
+        myItems().add(newItem);
+        return true;
+    }
+    
+    public String getItems()
+{
+    String allItems = myItems().toString();
+    return allItems.substring(1, allItems.length() - 1);
+}
+    
+    
+    
+    
+    
+   
+    
+    
+    
+    
+        
+    
+
+    
 }
